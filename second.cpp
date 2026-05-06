00873defe592104f1bc9fcbc3002b300b3cf9c4c
unknown <shisir.kafle@gmail.com>
2026-05-06 06:54:32 +0545

    Done Second Program

---
+++ second.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter x and y:\n";
+    cin>>x>>y;
+    int sum = x+y;
+    cout<<"The sum of x and y is:  "<<sum<<endl;
+    return EXIT_SUCCESS;
+}
