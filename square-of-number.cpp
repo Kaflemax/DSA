4efdaa517fb7502d83b931a2f4f16b72fa09770a
Kaflemax <shisir.kafle@gmail.com>
2026-05-15 10:44:45 +0545

    Done first question

---
+++ square-of-number.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int n,num;
+     cout<<"Enter your number ";
+     cin>>n;
+     num=n*n;
+     cout<<"The square root of a number is "<<num<<endl;
+     return 0;
+}
