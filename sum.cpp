49af5411c15e7eef7d59a395e7a2c847de2f4445
Kaflemax <shisir.kafle@gmail.com>
2026-05-15 15:38:56 +0545

    Done second question

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int a,b,sum;
+     cout<<"Enter first number ";
+     cin>>a;
+     cout<<"Enter second number ";
+     cin>>b;
+     sum=a+b;
+     cout<<"The square root of a number is "<<sum<<endl;
+     return 0;
+}
